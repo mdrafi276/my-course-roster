b53bf0eae4baf9a99bdc0ab558f33e496ef9e7c1
mdrafi276 <mdrafi37473@gmail.com>
2023-09-15 08:33:13 -0700

    totle section and alert added commit

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,5 +1,11 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 
+const Cart = ({ selectrdCourse, allTotal, credit, remaining }) => {
+  return (
+    <div className=" bg-white p-5 rounded-[10px]  w-full text-left   md:w-[250px] lg:w-[312px] gap-5">
+    
+  );
+};
 
 export default Cart;
