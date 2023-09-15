966ef88fdf18b151499047ba61a6a676efbdd227
mdrafi276 <mdrafi37473@gmail.com>
2023-09-15 08:35:56 -0700

    remaning section added commit

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,7 +4,14 @@
 const Cart = ({ selectrdCourse, allTotal, credit, remaining }) => {
   return (
     <div className=" bg-white p-5 rounded-[10px]  w-full text-left   md:w-[250px] lg:w-[312px] gap-5">
-    
+      <h1 id="creadit-hour" className=" border-b-2   pb-5 text-[#2F80ED]">
+        Credit Hour Remaining {remaining} hr
+      </h1>
+      <h2 className="pt-4  " id="course-name">Course Name</h2>
+
+
+
+   
   );
 };
 
